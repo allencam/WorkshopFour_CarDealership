f5940eb93b16d46ad10a4a6e86dae3878603f061
Camron Allen <allencam093@gmail.com>
2024-10-24 15:01:47 -0500

    Class setup

---
+++ src/main/java/com/ps/Dealership.java
@@ -0,0 +1,39 @@
+package com.ps;
+
+import java.util.ArrayList;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+
+    public static ArrayList<Vehicle> inventory = new ArrayList<>();
+
+    public static void getVehiclesByPrice(double min, double max) {
+
+    }
+    public static void getVehiclesByMakeModel(String make, String model) {
+
+    }
+    public static void getVehiclesByYear(int min, int max) {
+
+    }
+    public static void getVehiclesByColor(String color) {
+
+    }
+    public static void getVehiclesByMileage(int min, int max) {
+
+    }
+    public static void getVehiclesByType(String vehicleType) {
+
+    }
+    public static void getAllVehicles() {
+
+    }
+    public static void addVehicle(Vehicle vehicle) {
+
+    }
+    public static void removeVehicle(Vehicle vehicle) {
+
+    }
+}
---
+++ src/main/java/com/ps/UserInterface.java
@@ -0,0 +1,13 @@
+package com.ps;
+
+public class UserInterface {
+    // private static Dealership dealership
+
+    private static void init(){ // private, since we don't want to access this method in any other class
+        // Loading the dealership from a file
+    }
+    public static void display() {
+        init();
+        // Load the menu
+    }
+}
